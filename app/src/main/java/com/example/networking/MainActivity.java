7726a7c208af01d3965a69f9fc39241dcce4db02
a18fayha <a18fayha@student.his.se>
2022-05-05 09:25:24 +0200

    - provided internet permissions to manifest file - Implemeted MainActivity so that Json reads URL instead of file

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -20,7 +20,7 @@ import java.util.concurrent.BlockingDeque;
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
 
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
     private ArrayList<Mountain> mountainList;
     private RecyclerViewAdapter adapter;
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
 
 
 
-        new JsonFile(this, this).execute(JSON_FILE);
+        new JsonTask(MainActivity.this).execute(JSON_URL);
     }
 
     @Override
